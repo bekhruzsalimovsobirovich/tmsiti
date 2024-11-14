5fc37efa611cdb46386dd03d61c289d3cc6ab280
bekhruzsalimovsobirovich <bekhruzsalimovsobirovich@gmail.com>
2024-11-14 18:01:19 +0500

    first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex">
+    <div class="login">
+      <div class="top">
+        <h3>Platformaga kirish</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem delectus distinctio enim expedita explicabo illo ratione similique tenetur, veritatis voluptatem.</p>
+      </div>
+      <div class="bottom">
+        <form action="">
+
+        </form>
+      </div>
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
