630a5653cb02419cddab4d32ab896c30813d3760
glpat-5_pov_urCAG7DRKNkMs2 <glpat-5_pov_urCAG7DRKNkMs2>
2024-11-16 09:48:26 +0500

    login page responsive ready

--- src/components/auth/LoginComponent.vue
+++ src/components/auth/LoginComponent.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="flex justify-center h-[100vh] w-full">
-    <div class="login flex flex-col text-center justify-center w-[40%] justify-center items-center">
-      <div class="w-[450px]">
+
+    <div class="login flex flex-col text-center justify-center w-[70%] md:w-[40%] justify-center items-center">
+      <div class="w-full md:w-[450px]">
         <div class="top mb-[30px]">
-          <h1 class="font-bold text-[24px] mb-[5px]">Platformaga kirish</h1>
-          <p class="color-[#393C4E] text-center leading-[24px]">
+          <h1 class="font-bold text-[18px] md:text-[24px] mb-[5px]">Platformaga kirish</h1>
+          <p class="color-[#393C4E] text-center leading-[18px] md:leading-[24px]">
             Loyiha-smeta hujjatlarini ekspertizadan o'tkazish axborot tizimiga xush kelibsiz
           </p>
         </div>
         <div class="bottom">
-          <div class="buttons flex items-center justify-between mb-[30px]">
+          <div class="buttons flex items-center justify-between gap-[20px] mb-[30px]">
             <button
               v-for="(tab,index) in tabs"
               :key="index"
@@ -18,19 +19,19 @@
                 'text-[#000] border-[#01565B]' : activeTab === index,
                 'text-[#595959] border-[#595959]' :activeTab !== index
               }"
-              class="btn w-[134px] h-[40px] py-[8px] px-[12px] border-[1px] rounded-[5px] bg-[#fff]"
+              class="btn w-full md:w-[134px] h-[40px] py-[8px] px-[8px] md:px-[12px] border-[1px] rounded-[5px] text-[14px] bg-[#fff]"
             >{{tab}}</button>
           </div>
           <div class="form">
             <form class="flex flex-col">
               <div v-if="activeTab === 0">
                 <div class="flex flex-col text-start mb-[15px]">
-                  <label for="login" class="mb-[5px] font-[400] leading-[24px]">Login</label>
-                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]" id="login" type="text" placeholder="Loginni kiriting">
+                  <label for="login" class="mb-[5px] font-[400] leading-[18px] text-[14px] md:text-[16px] md:leading-[24px]">Login</label>
+                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0] text-[14px] md:text-[16px]" id="login" type="text" placeholder="Loginni kiriting">
                 </div>
                 <div class="flex flex-col text-start mb-[35px]">
-                  <label for="password" class="mb-[5px] font-[400] leading-[24px]">Parol</label>
-                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]" id="password" type="password" placeholder="Parolni kiriting">
+                  <label for="password" class="mb-[5px] font-[400] leading-[18px] text-[14px] md:text-[16px] md:leading-[24px]">Parol</label>
+                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0] text-[14px] md:text-[16px]" id="password" type="password" placeholder="Parolni kiriting">
                 </div>
                 <div class="flex flex-col">
                   <button class="rounded-[8px] mb-[20px] text-[#fff] leading-[20px] text-[14px] font-[500] bg-[#01565B] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]">Kirish</button>
@@ -39,18 +40,18 @@
               </div>
               <div v-if="activeTab === 1">
                 <div class="flex flex-col text-start mb-[15px]">
-                  <label for="phone" class="mb-[10px] font-[400] leading-[24px]">Telefon raqam</label>
-                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]" id="phone" type="text" placeholder="Telefon raqamni kiriting">
+                  <label for="phone" class="mb-[10px] font-[400] text-[14px] leading-[18px] md:text-[16px] md:leading-[24px]">Telefon raqam</label>
+                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0] text-[14px] md:text-[16px] md:leading-[24px]" id="phone" type="text" placeholder="Telefon raqamni kiriting">
                 </div>
                 <div class="flex flex-col">
-                  <p class="text-[#8c8c8c] mb-[20px] text-start font-[400] text-[14px] leading-[20px]">Tizimga kirish uchun Mobile-ID xizmati telefon raqamingizga ulangan bo'lishi kerak.</p>
-                  <button class="rounded-[8px] mb-[20px] text-[#fff] leading-[20px] text-[14px] font-[500] bg-[#01565B] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]">SMS kodni olish</button>
+                  <p class="text-[#8c8c8c] mb-[20px] text-start font-[400] text-[14px] leading-[18px] md:leading-[20px]">Tizimga kirish uchun Mobile-ID xizmati telefon raqamingizga ulangan bo'lishi kerak.</p>
+                  <button class="rounded-[8px] mb-[20px] text-[#fff] leading-[18px] md:leading-[20px] text-[14px] font-[500] bg-[#01565B] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]">SMS kodni olish</button>
                 </div>
               </div>
               <div v-if="activeTab === 2">
                 <div class="flex flex-col text-start mb-[15px]">
-                  <label for="eri" class="mb-[5px] font-[400] leading-[24px]">Eri kaliti</label>
-                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]" id="eri" type="text" placeholder="ERI kalitini kiriting">
+                  <label for="eri" class="mb-[5px] font-[400] text-[14px] leading-[18px] md:text-[16px] md:leading-[24px]">Eri kaliti</label>
+                  <input class="border-[1px] rounded-[5px] py-[8px] outline-none px-[12px] rounded-[#f0f0f0] text-[14px] md:text-[16px]" id="eri" type="text" placeholder="ERI kalitini kiriting">
                 </div>
                 <div class="flex flex-col">
                   <button class="rounded-[8px] mb-[20px] text-[#fff] leading-[20px] text-[14px] font-[500] bg-[#01565B] py-[8px] outline-none px-[12px] rounded-[#f0f0f0]">Kirish</button>
@@ -61,9 +62,11 @@
         </div>
       </div>
     </div>
-    <div class="content bg-[] w-[60%]">
+
+    <div class="content bg-[] w-[60%] hidden xl:block">
       <img class="h-[100vh] w-full object-cover" src="/public/images/architecture-company.webp" alt="architect">
     </div>
+
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'xs':'320px',
+        'sm': '640px',
+        'md':'870px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        '3xl': '1920px'
+      },
+    },
   },
   variants: {
     extend: {},
